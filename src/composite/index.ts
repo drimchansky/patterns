26bdf0edc12c2934fcbb8f6a47e2dd8477d62ae6
Nikita Chernov <drimchansky@gmail.com>
2021-11-06 12:22:36 +0300

    composite

---
+++ src/composite/index.ts
@@ -0,0 +1,120 @@
+/**
+ * Composite is a structural design pattern that lets you compose
+ * objects into tree structures and then work with these
+ * structures as if they were individual objects.
+ * 
+ * Applicability:
+ * - When you have to implement a tree-like object structure
+ * - When you want the client code to treat both simple and complex elements uniformly
+ */
+
+/** */
+abstract class Component {
+
+    protected parent: Component
+
+    /** */
+    public setParent(parent: Component) {
+        this.parent = parent
+    }
+
+    /** */
+    public getParent(): Component {
+        return this.parent
+    }
+
+    /** */
+    public add(_component: Component): void { }
+
+    /** */
+    public remove(_component: Component): void { }
+
+    /** */
+    public isComposite(): boolean {
+        return false
+    }
+
+    /** */
+    public abstract operation(): string;
+}
+
+/** */
+class Leaf extends Component {
+
+    /** */
+    public operation(): string {
+        return 'Leaf'
+    }
+}
+
+/** */
+class Composite extends Component {
+
+    protected children: Component[] = [];
+
+    /** */
+    public add(component: Component): void {
+        this.children.push(component)
+        component.setParent(this)
+    }
+
+    /** */
+    public remove(component: Component): void {
+        const componentIndex = this.children.indexOf(component)
+        this.children.splice(componentIndex, 1)
+
+        component.setParent(null)
+    }
+
+    /** */
+    public isComposite(): boolean {
+        return true
+    }
+
+    /** */
+    public operation(): string {
+        const results = []
+        for (const child of this.children) {
+            results.push(child.operation())
+        }
+
+        return `Branch(${results.join('+')})`
+    }
+}
+
+/** */
+function clientCodeCO(component: Component) {
+    // ...
+    console.log(`RESULT: ${component.operation()}`)
+    // ...
+}
+
+const simple = new Leaf()
+console.log('Client: I\'ve got a simple component:')
+clientCodeCO(simple)
+console.log('')
+
+const tree = new Composite()
+const branch1 = new Composite()
+branch1.add(new Leaf())
+branch1.add(new Leaf())
+const branch2 = new Composite()
+branch2.add(new Leaf())
+tree.add(branch1)
+tree.add(branch2)
+console.log('Client: Now I\'ve got a composite tree:')
+clientCodeCO(tree)
+console.log('')
+
+/** */
+function clientCodeCO2(component1: Component, component2: Component) {
+    // ...
+    if (component1.isComposite()) {
+        component1.add(component2)
+    }
+    console.log(`RESULT: ${component1.operation()}`)
+    // ...
+}
+
+console.log('Client: I don\'t need to check the components classes even when managing the tree:')
+clientCodeCO2(tree, simple)
